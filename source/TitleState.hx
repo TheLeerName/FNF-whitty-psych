86c858e2c1a2aefeb2baf66fd55799fd7269cac0
TheLeerName <lesha.nechaev.05@mail.ru>
2021-11-01 00:40:25 +0700

    title menu with bomb funni

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -203,10 +203,9 @@ class TitleState extends MusicBeatState
 
 		swagShader = new ColorSwap();
 		if(!FlxG.save.data.psykaEasterEgg || !easterEggEnabled) {
-			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-			gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-			gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-			gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+			gfDance = new FlxSprite(FlxG.width * 0.55, FlxG.height * 0.07);
+			gfDance.frames = Paths.getSparrowAtlas('whittyDanceTitle');
+			gfDance.animation.addByPrefix('dance', 'Whitty Dancing Beat', 30, false);
 		}
 		else //Psyka easter egg
 		{
@@ -449,13 +448,18 @@ class TitleState extends MusicBeatState
 		if(logoBl != null) 
 			logoBl.animation.play('bump');
 
-		if(gfDance != null) {
-			danceLeft = !danceLeft;
-
-			if (danceLeft)
-				gfDance.animation.play('danceRight');
+		if (gfDance != null)
+		{
+			if (!FlxG.save.data.psykaEasterEgg || !easterEggEnabled)
+				gfDance.animation.play('dance');
 			else
-				gfDance.animation.play('danceLeft');
+			{
+				danceLeft = !danceLeft;
+				if (danceLeft)
+					gfDance.animation.play('danceRight');
+				else
+					gfDance.animation.play('danceLeft');
+			}
 		}
 
 		if(!closedState) {
