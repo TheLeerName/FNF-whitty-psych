6a1c0582e0ca16d18f67faa4c1f2f7198f36ba91
TheLeerName <lesha.nechaev.05@mail.ru>
2021-11-04 01:40:10 +0700

    achievement things

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3316,8 +3316,7 @@ class PlayState extends MusicBeatState
 		if(achievementObj != null) {
 			return;
 		} else {
-			var achieve:String = checkForAchievement(['week1_nomiss', 'week2_nomiss', 'week3_nomiss', 'week4_nomiss',
-				'week5_nomiss', 'week6_nomiss', 'week7_nomiss', 'bonusweek_nomiss', 'ballisticold_nomiss', 'ur_bad',
+			var achieve:String = checkForAchievement(['bonusweek_nomiss', 'ballisticold_nomiss', 'ur_bad',
 				'ur_good', 'hype', 'two_keys', 'toastie', 'debugger']);
 
 			if(achieve != null) {
@@ -4394,27 +4393,15 @@ class PlayState extends MusicBeatState
 				var unlock:Bool = false;
 				switch(achievementName)
 				{
-					case 'week1_nomiss' | 'week2_nomiss' | 'week3_nomiss' | 'week4_nomiss' | 'week5_nomiss' | 'week6_nomiss' | 'bonusweek_nomiss':
+					case 'bonusweek_nomiss' | 'ballisticold_nomiss':
 						if(isStoryMode && campaignMisses + songMisses < 1 && CoolUtil.difficultyString() == 'HARD' && storyPlaylist.length <= 1 && !changedDifficulty && !usedPractice)
 						{
 							var weekName:String = WeekData.getWeekFileName();
 							switch(weekName) //I know this is a lot of duplicated code, but it's easier readable and you can add weeks with different names than the achievement tag
 							{
 								case 'week1':
-									if(achievementName == 'week1_nomiss') unlock = true;
-								case 'week2':
-									if(achievementName == 'week2_nomiss') unlock = true;
-								case 'week3':
-									if(achievementName == 'week3_nomiss') unlock = true;
-								case 'week4':
-									if(achievementName == 'week4_nomiss') unlock = true;
-								case 'week5':
-									if(achievementName == 'week5_nomiss') unlock = true;
-								case 'week6':
-									if(achievementName == 'week6_nomiss') unlock = true;
-								case 'week7':
 									if(achievementName == 'bonusweek_nomiss') unlock = true;
-								case 'week8':
+								case 'week2':
 									if(achievementName == 'ballisticold_nomiss') unlock = true;
 							}
 						}
